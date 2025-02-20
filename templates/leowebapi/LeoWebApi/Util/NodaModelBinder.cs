dc6218e7be55d8ea8ac163b77082c6c0a706a5a1
Markus Haslinger <markus@haslinger.it>
2025-02-20 16:35:25 +0100

    improve naming

--- templates/leowebapi/LeoWebApi/Util/NodaModelBinder.cs
+++ templates/leowebapi/LeoWebApi/Util/NodaModelBinder.cs
@@ -3,51 +3,6 @@ using NodaTime.Text;
 
 namespace LeoWebApi.Util;
 
-public sealed class NodaModelBinder : NodaTimeModelBinder<LocalDate>
-{
-    protected override ParseResult<LocalDate> Parse(string dateString) => LocalDatePattern.Iso.Parse(dateString);
-}
-
-public sealed class InstantModelBinder : NodaTimeModelBinder<Instant>
-{
-    protected override ParseResult<Instant> Parse(string dateString) => InstantPattern.ExtendedIso.Parse(dateString);
-}
-
-public abstract class NodaTimeModelBinder<T> : IModelBinder
-{
-    public Task BindModelAsync(ModelBindingContext bindingContext)
-    {
-        var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
-
-        if (valueProviderResult == ValueProviderResult.None)
-        {
-            return Task.CompletedTask;
-        }
-
-        string? dateString = valueProviderResult.FirstValue;
-        if (string.IsNullOrWhiteSpace(dateString))
-        {
-            bindingContext.ModelState.TryAddModelError(bindingContext.ModelName, "Empty date");
-
-            return Task.CompletedTask;
-        }
-
-        ParseResult<T> parseResult = Parse(dateString);
-        if (!parseResult.Success)
-        {
-            bindingContext.ModelState.TryAddModelError(bindingContext.ModelName, "Invalid date format");
-
-            return Task.CompletedTask;
-        }
-
-        bindingContext.Result = ModelBindingResult.Success(parseResult.Value);
-
-        return Task.CompletedTask;
-    }
-
-    protected abstract ParseResult<T> Parse(string dateString);
-}
-
 public sealed class NodaTimeModelBinderProvider : IModelBinderProvider
 {
     public IModelBinder? GetBinder(ModelBinderProviderContext context)
@@ -56,9 +11,54 @@ public sealed class NodaTimeModelBinderProvider : IModelBinderProvider
         {
             return new InstantModelBinder();
         }
-
+        
         return context.Metadata.ModelType == typeof(LocalDate)
-            ? new NodaModelBinder()
-            : null;
+        ? new LocalDateModelBinder()
+        : null;
     }
+}
+
+file abstract class NodaTimeModelBinder<T> : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+        
+        if (valueProviderResult == ValueProviderResult.None)
+        {
+            return Task.CompletedTask;
+        }
+        
+        string? dateString = valueProviderResult.FirstValue;
+        if (string.IsNullOrWhiteSpace(dateString))
+        {
+            bindingContext.ModelState.TryAddModelError(bindingContext.ModelName, "Empty date");
+            
+            return Task.CompletedTask;
+        }
+        
+        ParseResult<T> parseResult = Parse(dateString);
+        if (!parseResult.Success)
+        {
+            bindingContext.ModelState.TryAddModelError(bindingContext.ModelName, "Invalid date format");
+            
+            return Task.CompletedTask;
+        }
+        
+        bindingContext.Result = ModelBindingResult.Success(parseResult.Value);
+        
+        return Task.CompletedTask;
+    }
+    
+    protected abstract ParseResult<T> Parse(string dateString);
+}
+
+file sealed class LocalDateModelBinder : NodaTimeModelBinder<LocalDate>
+{
+    protected override ParseResult<LocalDate> Parse(string dateString) => LocalDatePattern.Iso.Parse(dateString);
+}
+
+file sealed class InstantModelBinder : NodaTimeModelBinder<Instant>
+{
+    protected override ParseResult<Instant> Parse(string dateString) => InstantPattern.ExtendedIso.Parse(dateString);
 }
