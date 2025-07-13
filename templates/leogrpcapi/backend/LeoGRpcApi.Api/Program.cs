cd472e78f3471d3f129e21cd605a965036a8d55d
Markus Haslinger <markus@haslinger.it>
2025-07-13 11:10:01 +0200

    minor template fixes

--- templates/leogrpcapi/backend/LeoGRpcApi.Api/Program.cs
+++ templates/leogrpcapi/backend/LeoGRpcApi.Api/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(o =>
 {
     // not using HTTPS, because all production backends _have_ to be behind a reverse proxy which will handle SSL termination
-    // h2c endpoint – no TLS 
     o.ListenLocalhost(5200, listenOpts =>
                           listenOpts.Protocols = HttpProtocols.Http2);
 });
