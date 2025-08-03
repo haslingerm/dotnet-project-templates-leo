932aea8fbeaf00c5a1f7df6d9a8ba5e6840c32a4
Markus Haslinger <markus@haslinger.it>
2025-08-03 11:36:37 +0200

    remove redundant files

--- templates/leogrpcapi/backend/LeoGRpcApi.Api.Shared/Const.cs
+++ templates/leogrpcapi/backend/LeoGRpcApi.Api.Shared/Const.cs
@@ -3,5 +3,4 @@ namespace LeoGRpcApi.Api.Shared;
 public static class Const
 {
     public static readonly DateTimeZone TimeZone = DateTimeZoneProviders.Tzdb["Europe/Vienna"];
-    public const int RequiredDeltaVPerKg = 9_800;
 }
